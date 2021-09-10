434c72e32255e8f2e5677bf5b85fce16f3ce0df2
yoshiakiHirose223 <yoshiaki02231998@gmail.com>
2021-09-10 12:31:20 +0900

    first commit

---
+++ edit.php
@@ -0,0 +1,26 @@
+<?php
+require_once('functions.php');
+setToken();
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>編集</title>
+</head>
+<body>
+  <?php if (!empty($_SESSION['err'])): ?>
+    <p><?= $_SESSION['err']; ?></p>
+  <?php endif; ?>
+  <form action="store.php" method="post">
+    <input type="hidden" name="id" value="<?= e($_GET['id']); ?>">
+    <input type="text" name="content" value="<?= e(getSelectedTodo($_GET['id'])); ?>">
+    <input type="hidden" name="token" value="<?= $_SESSION['token']; ?>">
+    <input type="submit" value="更新">
+  </form>
+  <div>
+    <a href="index.php">一覧へもどる</a>
+  </div>
+  <?php unsetError(); ?>
+</body>
+</html>
