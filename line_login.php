4ad7f48c7472597fc134e8314a6f0dc17e1f1a4c
yoshiakiHirose223 <yoshiaki02231998@gmail.com>
2021-09-10 16:04:56 +0900

    [update]LOGIN_URL定数の文字列結合修正 - . の書き忘れ

--- line_login.php
+++ line_login.php
@@ -6,9 +6,9 @@ define('SCOPE', 'profile%20openid%20email');
 
 define('LOGIN_URL', 'https://access.line.me/oauth2/v2.1/authorize?response_type=code' .
 '&client_id='. CLIENT_ID
-'&redirect_uri=' . REDIRECT_URL
-'&state=' . STATE
-'&scope=' . SCOPE
+. '&redirect_uri=' . REDIRECT_URL
+. '&state=' . STATE
+. '&scope=' . SCOPE
 );
 
 function getState()
