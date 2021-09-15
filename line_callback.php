eb0e558a498140575cd8e0e502feb3161ae07735
yoshiakiHirose223 <yoshiaki02231998@gmail.com>
2021-09-15 17:25:57 +0900

    [update]不要な記述を削除

--- line_callback.php
+++ line_callback.php
@@ -6,27 +6,7 @@ require_once('connection.php');
 if ($_GET['state'] === $_SESSION['state']) {
     $tokenJson = requestTokenJson();
     $checkedIdTokenJson = checkIdToken($tokenJson->id_token);
-    createUser($checkedIdTokenJson->aud);
-    echo('新規登録完了');
+    // createUser($checkedIdTokenJson->aud);
+    $_SESSION['user_id'] = getUserIdByAud($checkedIdTokenJson->aud);
+    header('Location: ./index.php');
 }
-
-
-$accessToken = $tokenJson->access_token;
-$idToken = $tokenJson->id_token;
-$refreshToken = $tokenJson->refresh_token;
-
-$getProfileUrl = 'https://api.line.me/oauth2/v2.1/verify';
-$profileParam = [
-    'id_token'  => $idToken,
-    'client_id' => CLIENT_ID,
-];
-
-$profileCh = curl_init();
-curl_setopt($profileCh, CURLOPT_URL, $getProfileUrl);
-curl_setopt($profileCh, CURLOPT_POST, true);
-curl_setopt($profileCh, CURLOPT_POSTFIELDS, http_build_query($profileParam));
-curl_setopt($profileCh, CURLOPT_RETURNTRANSFER, true);
-$profileResponse = curl_exec($profileCh);
-curl_close($profileCh);
-
-$profileJson = json_decode($profileResponse);
