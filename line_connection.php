5d8134a87ec04cebdafa74462e6d608dc9db2edb
yoshiakiHirose223 <yoshiaki02231998@gmail.com>
2021-09-15 14:57:10 +0900

    [update]IDトークンの検証処理追加 - 変数名一部修正

--- line_connection.php
+++ line_connection.php
@@ -5,7 +5,7 @@ require_once('line_config.php');
 function requestTokenJson()
 {
     $requestTokenUrl = 'https://api.line.me/oauth2/v2.1/token';
-    $tokenParam = [
+    $param = [
         'grant_type'    => 'authorization_code',
         'code'          => $_GET['code'],
         'redirect_uri'  => REDIRECT_URL,
@@ -14,14 +14,33 @@ function requestTokenJson()
         'code_verifier' => $_SESSION['code_verifier'],
     ];
 
-    $tokenCh = curl_init();
-    curl_setopt($tokenCh, CURLOPT_URL, $requestTokenUrl);
-    curl_setopt($tokenCh, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
-    curl_setopt($tokenCh, CURLOPT_POST, true);
-    curl_setopt($tokenCh, CURLOPT_POSTFIELDS, http_build_query($tokenParam));
-    curl_setopt($tokenCh, CURLOPT_RETURNTRANSFER, true);
-    $tokenResponse = curl_exec($tokenCh);
-    curl_close($tokenCh);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $requestTokenUrl);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/x-www-form-urlencoded'));
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($param));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $tokenResponse = curl_exec($ch);
+    curl_close($ch);
+
+    return json_decode($tokenResponse);
+}
+
+function checkIdToken($idToken)
+{
+    $checkIdTokenUrl = 'https://api.line.me/oauth2/v2.1/verify';
+    $param = [
+        'id_token' => $idToken,
+        'client_id' => CLIENT_ID,
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $checkIdTokenUrl);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($param));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $tokenResponse = curl_exec($ch);
+    curl_close($ch);
 
     return json_decode($tokenResponse);
 }
